e77dce29ebb59419c527b05b6ece22bfaba267a4
Maksim Shin <maxim.shinl@gmail.com>
2026-02-03 17:46:09 +0500

    if else statement

--- js/script.js
+++ js/script.js
@@ -1,44 +1,76 @@
+const toggleBtn = document.getElementById('toggleBtn');
+const wrapper = document.querySelector('.swiper-wrapper');
+let swiperInstance = null;
 
-// SWIPER
-const swiper = new Swiper('.swiper', {
-  loop: false,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  spaceBetween: 16,
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      grid: {
-        rows: 2,
-        fill: 'row',
-      },
+if (window.innerWidth < 768) {
+  if (!swiperInstance) {
+    swiperInstance = new Swiper('.swiper', {
+      slidesPerView: 'auto',
       spaceBetween: 16,
-      allowTouchMove: false,
-      centeredSlides: false,
-    },
-  },
+      centeredSlide: true,
+      slidesPerGroup: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+  toggleBtn.style.display = 'none';
+  wrapper.classList.remove('is-open');
+
+
+  if (swiperInstance) {
+    swiperInstance.destroy(true, true);
+    swiperInstance = null;
+  }
+  
+  
+  if (wrapper.scrollHeight > wrapper.clientHeight) {
+    toggleBtn.style.display = 'flex';
+  } else {
+    toggleBtn.style.display = 'none';
+    wrapper.classList.remove('is-open');
+  }
+}
+
+
+toggleBtn.addEventListener('click', () => {
+  if (wrapper.classList.contains('is-open')) {
+    wrapper.classList.remove('is-open');
+    toggleBtn.innerHTML = '<img src="./brands_logo/expand.svg" alt="">Показать все';
+  } else {
+    wrapper.classList.add('is-open');
+    toggleBtn.innerHTML = '<img src="./brands_logo/expand.svg" alt="">Скрыть';
+  }
 });
 
 
-// BRANDS GRID
-const toggleBtn = document.getElementById('toggleBtn');
-const brandsGrid = document.querySelector('.brands__grid');
-
-toggleBtn.addEventListener ('click', () => {
-  const expanded = brandsGrid.classList.toggle('full__grid');
-
-  if (expanded) {
-    
-    toggleBtn.innerHTML = '<img src="../brands_logo/expand.svg" alt="">Скрыть';
-
+window.addEventListener('resize', () => {
+ 
+  if (window.innerWidth < 768) {
+    if (!swiperInstance) {
+      swiperInstance = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
+    }
+    toggleBtn.style.display = 'none';
+    wrapper.classList.remove('is-open');
   } else {
-  
-    toggleBtn.innerHTML = '<img src="../brands_logo/expand.svg" alt="">Показать все';
+    if (swiperInstance) {
+      swiperInstance.destroy(true, true);
+      swiperInstance = null;
+    }
+
+    if (wrapper.scrollHeight > wrapper.clientHeight) {
+      toggleBtn.style.display = 'flex';
+    } else {
+      toggleBtn.style.display = 'none';
+      wrapper.classList.remove('is-open');
+    }
   }
 });
