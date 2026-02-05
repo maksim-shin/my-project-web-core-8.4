95efa6680845fc2f0891ff4dc4be7aae9ead803c
Maksim Shin <maxim.shinl@gmail.com>
2026-02-06 01:48:23 +0500

    justify mobile content

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ const wrapper = document.querySelector('.swiper-wrapper');
 let swiperInstance = null;
 
 if (window.innerWidth < 768) {
+  // Если слайдер не создан — создаём
   if (!swiperInstance) {
     swiperInstance = new Swiper('.swiper', {
+      centeredSlides: true,
       slidesPerView: 'auto',
       spaceBetween: 16,
-     
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -16,14 +17,13 @@ if (window.innerWidth < 768) {
   }
   toggleBtn.style.display = 'none';
   wrapper.classList.remove('is-open');
-
-
+} else {
+  // При ширине 768 и выше слайдер уничтожаем
   if (swiperInstance) {
     swiperInstance.destroy(true, true);
     swiperInstance = null;
   }
-  
-  
+  // Показываем кнопку, если содержимое больше по высоте
   if (wrapper.scrollHeight > wrapper.clientHeight) {
     toggleBtn.style.display = 'flex';
   } else {
@@ -31,7 +31,6 @@ if (window.innerWidth < 768) {
     wrapper.classList.remove('is-open');
   }
 }
-
 
 toggleBtn.addEventListener('click', () => {
   if (wrapper.classList.contains('is-open')) {
@@ -43,9 +42,7 @@ toggleBtn.addEventListener('click', () => {
   }
 });
 
-
 window.addEventListener('resize', () => {
- 
   if (window.innerWidth < 768) {
     if (!swiperInstance) {
       swiperInstance = new Swiper('.swiper', {
@@ -64,7 +61,6 @@ window.addEventListener('resize', () => {
       swiperInstance.destroy(true, true);
       swiperInstance = null;
     }
-
     if (wrapper.scrollHeight > wrapper.clientHeight) {
       toggleBtn.style.display = 'flex';
     } else {
